0939b8b6adb35acb9bd7c1d50290a65556b0a93a
GitFutureTrunks <145227152+GitFutureTrunks@users.noreply.github.com>
2023-11-19 16:22:57 -0600

    Update main.lua

--- server/main.lua
+++ server/main.lua
@@ -1,1 +1,13 @@
+if Config.Utility.Framework == "NEW" then
+    QBCore = exports[Config.Utility.CoreName]:GetCoreObject()
+elseif Config.Utility.Framework == "OLD" then 
+    QBCore = nil
+    TriggerEvent(Config.Utility.CoreName, function(obj) QBCore = obj end) 
+else
+    print("The Framework '", Config.Utility.Framework, "' is not support, please change in config.lua")
+end
 
+QBCore.Functions.CreateUseableItem("wallet", function(source, item)
+    local Player = QBCore.Functions.GetPlayer(source)
+    TriggerClientEvent('gft-wallet:Client:wallet', source, item.info.walletid)
+end)
