3b927e0bc1c1213855fabf6febca5adafc6f9363
GitFutureTrunks <145227152+GitFutureTrunks@users.noreply.github.com>
2023-11-19 16:17:32 -0600

    Update and rename fxmanifest to fxmanifest.lua

---
+++ fxmanifest.lua
@@ -0,0 +1,16 @@
+fx_version 'cerulean'
+author 'GitFutureTrunks'
+Description 'GFT-wallet'
+game 'gta5'
+
+shared_scripts { 
+    "config.lua",
+}
+
+server_script { 
+    "server/**.lua",
+}
+
+client_script {
+    "client/**.lua",
+}
